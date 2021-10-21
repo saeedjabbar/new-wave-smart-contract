e47293c5db7f9919c159e11811e28daa465bb567
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-10-20 23:15:09 -0400

    test production

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -67,31 +67,31 @@ contract WavePortal {
         /*
          * Generate a Psuedo random number between 0 and 100
          */
-        uint256 randomNumber = (block.difficulty + block.timestamp + seed) % 100;
-        console.log("Random # generated: %s", randomNumber);
+        //uint256 randomNumber = (block.difficulty + block.timestamp + seed) % 100;
+        //console.log("Random # generated: %s", randomNumber);
 
         /*
          * Set the generated, random number as the seed for the next wave
          */
-        seed = randomNumber;
+        //seed = randomNumber;
 
         /*
          * Give a 50% chance that the user wins the prize.
          */
-        if (randomNumber < 80) {
-            console.log("%s won!", msg.sender);
+        // if (randomNumber < 80) {
+        //     console.log("%s won!", msg.sender);
 
-            /*
-             * The same code we had before to send the prize.
-             */
-            uint256 prizeAmount = 0.0001 ether;
-            require(
-                prizeAmount <= address(this).balance,
-                "Trying to withdraw more money than the contract has."
-            );
-            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-            require(success, "Failed to withdraw money from contract.");
-        }
+        //     /*
+        //      * The same code we had before to send the prize.
+        //      */
+        //     uint256 prizeAmount = 0.0001 ether;
+        //     require(
+        //         prizeAmount <= address(this).balance,
+        //         "Trying to withdraw more money than the contract has."
+        //     );
+        //     (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+        //     require(success, "Failed to withdraw money from contract.");
+        // }
 
         emit NewWave(msg.sender, block.timestamp, _message);
 
